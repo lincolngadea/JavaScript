2319524941875b59543c98cbf0fd48638b1cb6de
Lincoln Gadéa <lincoln@glinc.com.br>
2020-09-27 20:55:21 -0300

    Adicionando eventos de teclado

--- projetos/calculadora-clone/scripts/controller/CalcController.js
+++ projetos/calculadora-clone/scripts/controller/CalcController.js
@@ -31,7 +31,51 @@ class CalcController{
         },1000);
 
         this.setLastNumberToDisplay();
+        this.initKeyboard();
 
+    }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+            
+            switch(e.key){
+                    case 'Escape':
+                    case 'Delete':
+                        this.clearAll();
+                        break;
+                    case 'Backspace':
+                        this.clearEntry();
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':                    
+                        this.addOperation(e.key);
+                       break
+
+                    case 'Enter':
+                        this.calc();
+                        break;
+                    case '.':
+                    case ',':
+                        this.addDot('.');
+                        break;
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        this.addOperation(e.key);
+                        break;  
+                }
+            }
+        );
     }
     addEventListenerAll(element, events, fn){
 
