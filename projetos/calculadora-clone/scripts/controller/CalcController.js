5d10f2f510a2a5e1290375799211e2dbbc9349bc
Lincoln Gadéa <lincoln@glinc.com.br>
2020-09-27 18:29:32 -0300

    adição do porcento correção de display início da configuração do 'igual'

--- projetos/calculadora-clone/scripts/controller/CalcController.js
+++ projetos/calculadora-clone/scripts/controller/CalcController.js
@@ -27,6 +27,8 @@ class CalcController{
            
         },1000);
 
+        this.setLastNumberToDisplay();
+
     }
     addEventListenerAll(element, events, fn){
 
@@ -48,7 +50,13 @@ class CalcController{
     }
 
     clearAll(){
+        this._operation = [];
+        this.setLastNumberToDisplay();
+    }
+
+    clearEntry(){
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     setLastOperation(value){
@@ -65,8 +73,11 @@ class CalcController{
                 break;
             }
         }
+
+        if(!lastNumber) lastNumber = 0;
         this.displayCalc = lastNumber;
     }
+
     getLastOperation(){
         return this._operation[this._operation.length-1];
     }
@@ -82,9 +93,23 @@ class CalcController{
         
     }
     calc(){
-        let last = this._operation.pop();
+
+        let last = "";
+
+        if(this._operation.length >3){
+            last = this._operation.pop();
+        }
+        
         let result = eval(this._operation.join(""));
-        this._operation =[result,last];
+
+        if(last =='%'){
+            result /= 100;
+            this._operation = [result];
+        }else{
+            this._operation =[result];
+            if(last) this._operation.push(last);
+        }
+       
         this.setLastNumberToDisplay();
     }
 
@@ -136,7 +161,7 @@ class CalcController{
                 this.addOperation('%');
                 break;
             case 'igual':
-                
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');
