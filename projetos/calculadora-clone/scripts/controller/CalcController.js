2dac6868b5fa86786834e18ff5d0ae82780333f2
Lincoln Gadéa <lincoln@glinc.com.br>
2020-09-27 17:31:46 -0300

    Validando o primeiro operador e calculando o resultado

--- projetos/calculadora-clone/scripts/controller/CalcController.js
+++ projetos/calculadora-clone/scripts/controller/CalcController.js
@@ -55,25 +55,49 @@ class CalcController{
         this._operation[this._operation.length-1] = value;
     }
 
+    setLastNumberToDisplay(value){
+
+    }
     getLastOperation(){
         return this._operation[this._operation.length-1];
     }
 
+    pushOperation(value){
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+
+           this.calc();
+           // console.log(this._operation);
+        }
+        calc(){
+            let last = this._operation.pop();
+            let result = eval(this._operation.join(""));
+            this._operation =[result,last];
+        }
+    }
+
     addOperation(value){
 
+        //  console.log('A',value, isNaN(this.getLastOperation()));
         if(isNaN(this.getLastOperation())){ 
             if(this.isOperator(value)){
                 this.setLastOperation(value);
             }else if(isNaN(value)){
-                console.log(value);
+                console.log('outra coisa',value);
             }else{
-                this._operation.push(value);
+                this.pushOperation(value);
             }
+        }else if(this.isOperator(value)){
+            this.pushOperation(value);
+
         }else{
             let newValue = this.getLastOperation().toString() + value.toString();
             this.setLastOperation(parseInt(newValue));
+
+            this.setLastNumberToDisplay();
         }       
-        console.log(this._operation);
+       // console.log(this._operation);
     }
 
     execBtn(value){
