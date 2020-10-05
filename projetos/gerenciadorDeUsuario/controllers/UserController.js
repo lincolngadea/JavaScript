10b77f62ba8f72325eacd49b0ef355669f819c5f
Lincoln Gadéa <lincoln@glinc.com.br>
2020-10-05 20:48:12 -0300

    refatoração - validação de formulário

--- projetos/gerenciadorDeUsuario/controllers/UserController.js
+++ projetos/gerenciadorDeUsuario/controllers/UserController.js
@@ -79,8 +79,17 @@ class UserController{
     getValues(){
 
         let user ={};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if(['name','email','password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+                
+            }
+
             if(field.name == "gender"){
                 if(field.checked){
                     user[field.name] = field.value;
@@ -92,7 +101,11 @@ class UserController{
             }
         });
         //console.log(user);
-    
+        if(!isValid){
+
+            return false;
+
+        }
         return new User(
             user.name,
             user.gender,
