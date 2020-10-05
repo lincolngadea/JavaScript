e13d1d9b4bfd7f67eca12f84a98089047aa2fe03
Lincoln Gadéa <lincoln@glinc.com.br>
2020-10-04 21:15:50 -0300

    refatoração para adicionar uma promise no UserCont

--- projetos/gerenciadorDeUsuario/controllers/UserController.js
+++ projetos/gerenciadorDeUsuario/controllers/UserController.js
@@ -16,24 +16,29 @@ class UserController{
 
             let values = this.getValues();
 
-            values.photo = ";"
+            this.getPhoto().then(
+                
+                (content)=>{
 
-            this.getPhoto((content)=>{
+                    values.photo = content;
 
-                values.photo = content;
+                    this.addLine(values);
+                }, 
+                (e)=>{
+                    console.log(e);
 
-                this.addLine(values);
+                }
+            );
 
-            });
-            
-           
-        
         });
     }
     //--------------FINAL ONSUBMIT-----------------<<
     //--------------INICIA GETPHOTO --------------->>
-    getPhoto(callback){
-        let fileReader = new FileReader();
+    getPhoto(){
+
+        return new Promise((resolve,reject)=>{
+
+            let fileReader = new FileReader();
 
         let elements = [...this.formEl.elements].filter(item=>{
 
@@ -46,10 +51,16 @@ class UserController{
         let file =elements[0].files[0];
 
         fileReader.onload = ()=>{
-            callback(fileReader.result);
+            resolve(fileReader.result);
         };
 
+        fileReader.onerror = (e)=>{
+            reject(e);
+        }
+
         fileReader.readAsDataURL(file);
+
+        });       
            
     }
     //--------------FINAL GETPHOTO-----------------<<
